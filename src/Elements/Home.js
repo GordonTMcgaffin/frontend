2db121a919055f6cc0fc3098e79ed392db2390e6
Mcgaffin, G, Mr [23565608@sun.ac.za] <23565608@sun.ac.za>
2022-06-04 22:16:54 +0200

    Lots of work done

--- src/Elements/Home.js
+++ src/Elements/Home.js
@@ -5,7 +5,14 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
 import { useNavigate } from 'react-router-dom';
+import {
+	useState,
+	useEffect
+} from 'react';
 
 import Image from '../Images/default_red.jpeg';
 import Background from '../Images/background2.jpg';
@@ -20,25 +27,19 @@ const Home = () => {
             backgroundRepeat: 'no-repeat',
             backgroundSize: 'cover',
             }}>
+            <AppBar position="static" style={{background: '#575759'}}>
+                <Toolbar color= "#FFFFFF">
+                    <Button 
+                        sx ={{color: '#FFFFFF',}}
+                        varient = "contained"
+                        onClick={() => navigate('/frontend/projects')}>
+                            Projects
+                    </Button>
+                </Toolbar>
+            </AppBar>
             <Grid container direct = "column">
-                <Grid item xs={12}>
-                        <Box sx= {{
-                            display: 'flex',
-                            height: '5vh',
-                            backgroundColor: '#575759',
-                        }} >
-                            <Button color= "success" varient = "contained" onClick={() => navigate('/frontend/projects')}>Projects</Button>
-                        </Box>
-                    </Grid> 
                 <Grid item xs={0.5}>
-                    <Box sx= {{
-                        height: '100vh',
-                        width: '4vh',
-                        backgroundColor: '#575759',
-                    }}
-                    >
-    
-                    </Box>
+
                 </Grid>
 
                 <Grid item xs={11.5}>
@@ -52,14 +53,18 @@ const Home = () => {
                         <img src = {Image} width='22%' />
                     </Grid>
                 
-                    <Grid item xs={12}>
+                    <Grid item xs={8}>
                         <br></br>
                         <Typography variant = 'h3' align = 'left' padding='5px' color = '#FFFFFF'>
                             <b>About Me</b>
                         </Typography>
                         <br></br>
                         <Typography variant = 'body1' align = 'left' padding = '5px' color = '#FFFFFF'>
-                            [To be done]
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet arcu erat. Nulla facilisi. Nullam vulputate, nisl porttitor feugiat luctus, nisi diam pharetra massa, et lobortis nulla lectus et magna. Donec tempus purus eget mattis egestas. Proin volutpat luctus ligula, ut tincidunt sapien cursus et. Praesent eleifend eros ac dui lobortis vulputate. Fusce pulvinar eros in magna laoreet iaculis. Donec tristique sapien vitae ultricies egestas. Sed egestas elit in ipsum congue, tincidunt rutrum arcu porttitor. Phasellus posuere tincidunt urna, vel egestas metus mattis in.
+                            Duis sed orci vulputate leo dictum pharetra. Etiam efficitur quam diam, sed lobortis odio dignissim sed. Maecenas orci sem, molestie id sapien ut, scelerisque tempus velit. Duis quis sem placerat, blandit ipsum ac, tincidunt ante. Vestibulum laoreet est ut elementum consectetur. Pellentesque feugiat rhoncus hendrerit. Mauris placerat molestie nunc ut lobortis.
+                            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur id neque diam. In eu turpis turpis. Pellentesque ullamcorper laoreet efficitur. Curabitur euismod blandit laoreet. Morbi rutrum sodales tortor a mollis. Pellentesque sed interdum nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean ac ultrices tortor, a suscipit nibh. Fusce at justo et diam ornare consectetur ac sed ipsum. Sed feugiat pulvinar rutrum. Vestibulum eu nisi commodo, commodo tortor eu, laoreet leo. Suspendisse eget nibh leo. Nulla egestas semper porttitor. Etiam elit turpis, facilisis at nisl in, tristique pulvinar felis.
+                            Fusce vitae mollis ex. Nunc mattis urna nec lorem cursus gravida. Aenean pharetra erat non odio dignissim convallis. Integer vel velit odio. In porttitor rhoncus ligula, eu molestie arcu aliquet eget. Integer mollis varius rhoncus. Suspendisse nec lorem nec nisl vehicula dapibus.
+                            Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam condimentum gravida velit ut facilisis. Nunc cursus odio in tortor elementum, sed porttitor lacus condimentum. Donec quis dapibus lacus. Integer malesuada rhoncus eros, a tristique nisi egestas sit amet. Pellentesque consectetur placerat sapien, a viverra nisi consectetur quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque vel felis quis ligula rutrum semper in sit amet dolor. Integer non convallis nunc. Donec dapibus libero posuere, faucibus quam id, ultrices ligula.
                         </Typography>
                     </Grid>
                 </Grid>
--- src/Elements/Projects.js
+++ src/Elements/Projects.js
@@ -1,10 +1,21 @@
-import ProjectBackground from '../Images/projects-background.jpg'
+import ProjectBackground from '../Images/projects-background_fade.jpg'
+import Traversal from '../Images/Traversal.png'
+import Warrior_Game from '../Images/Warrior_Game.png'
+import SD_GS from '../Images/SD_GS.png'
+import SD_NR from '../Images/SD_NR.png'
+import SD_ED from '../Images/SD_ED.png'
+import SD_SD from '../Images/SD_SD.png'
+
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
 
 const Projects = () => {
+
     const navigate = useNavigate();
     return (
             <div style = {{
@@ -14,6 +25,18 @@ const Projects = () => {
                 backgroundPosition: 'center',
                 position: 'relative',
                 }}>
+
+                <AppBar position="static" style={{background: '#575759'}}>
+                    <Toolbar color= "#FFFFFF">
+                        <Button 
+                            sx ={{color: '#FFFFFF',}}
+                            varient = "contained"
+                            onClick={() => navigate('/frontend/')}>
+                                Back
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+
                     <Grid container direct = "column">
                         <Grid item xs={12}>
                             <Typography variant = 'h1' color = '#FFFFFF' >
@@ -21,13 +44,79 @@ const Projects = () => {
                                 </Typography>
                         </Grid>
                         <Grid item xs={12}>
+                            <br></br>
+                        </Grid>
+                        <Grid item xs={8}>
                             <Typography variant = 'h3' color = '#FFFFFF' >
-                                    <b>[Projects will go here]</b>
-                                </Typography>
+                                <b>Traversal</b>
+                            </Typography>
+                            <Typography variant = 'p' color = '#FFFFFF' >
+                                [ToDo] (Copilot generated) Traversal is a game that I made for my CS class. It is a simple game where you can move around a grid and collect coins. The goal is to get to the end of the grid and collect the most coins. The game is designed to be played on a single screen and is designed to be played on a mobile device. The game is currently in development and is not yet playable.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <img align= 'left' src = {Traversal} width='50%' />
+                        </Grid>
+                        
+                        <Grid item xs={12}>
+                            <br></br>
+                        </Grid>
+                        
+                        <Grid item xs={4}>
+                            <img align= 'right' src = {Warrior_Game} width='50%' />
+                        </Grid> 
+
+                        <Grid item xs={8}>
+                            <Typography variant = 'h3' color = '#FFFFFF' >
+                                <b>Warrior Game</b>
+                            </Typography>
+                            <Typography variant = 'p' color = '#FFFFFF' >
+                                [ToDo] (Copilot generated) Warrior Game is a game that I created for the purpose of learning how to use the canvas element. The game is a simple game where the player can move around a grid and shoot enemies. The game is made with HTML, CSS, and Javascript. The game is currently in development.
+                            </Typography>
+                        </Grid>   
+
+                        <Grid item xs={12}>
+                            <br></br>
+                        </Grid>
+
+                        <Grid item xs={1}>
+                            <br></br>
+                        </Grid>
+                        <Grid item xs={5}>
+                            <Typography variant = 'h3' color = '#FFFFFF' >
+                                <b>Spot Detection</b>
+                            </Typography>
+                            <Typography variant = 'p' color = '#FFFFFF' >
+                                [ToDo] (Copilot generated) An application that detects the presence of a specific object in a video stream. The application is designed to be used with a webcam. The application is currently in development.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <br></br>
                         </Grid>
                         <Grid item xs={12}>
-                            <Button color= "success" varient = "contained" onClick={() => navigate('/frontend/')}>Back</Button>
+                            <br></br>
                         </Grid>
+                        <Grid item xs={4}>
+                            <img align= 'center' src = {SD_NR} width='65%' />
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <img align= 'center' src = {SD_ED} width='65%' />
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            <img align= 'center' src = {SD_SD} width='65%' />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <br></br>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <br></br>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <br></br>
+                        </Grid>
+
                     </Grid>
             </div>
     )
